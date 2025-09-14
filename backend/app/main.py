1aa9f40125b48b2efcec16cac53d59cf2b8b3621
jeico <jeicomorales1@gmail.com>
2025-09-13 20:06:49 -0600

    Actualiza main.py y requirements.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.api import domains, emails
 from app.api import auth, user, mailbox
 from app.db.base import engine, Base
 from app.core.database import connect_to_mongo, close_mongo_connection
 
+# IMPORTA TODAS LAS CLASES Y LA TABLA BRIDGE ANTES DE CREAR LAS TABLAS
+from app.models.user import User
+from app.models.role import Role
+from app.models.user_roles import user_roles
+from app.models.mailbox import Mailbox
+from app.models.outgoing_domain import OutgoingDomain
 
 app = FastAPI(
     title="Email Platform API",
@@ -26,22 +33,20 @@ app.include_router(mailbox.router)
 app.include_router(domains.router)
 app.include_router(emails.router)
 
-
 @app.get("/")
 def root():
     return {"service": "Email Platform API", "status": "online"}
-
 
 @app.on_event("startup")
 async def startup_event():
     try:
         await connect_to_mongo()
+        # INICIA LAS TABLAS TRAS importar todos los modelos
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
         print("✅ Tablas creadas/verificadas exitosamente y MongoDB conectado!")
     except Exception as e:
         print(f"❌ Error durante el startup: {e}")
-
 
 @app.on_event("shutdown")
 async def shutdown_event():
